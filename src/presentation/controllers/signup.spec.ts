28a6cce22d8c849ade0b9caa5d87642dc644b9b9
Steven Alves <stevenalves512@gmail.com>
2021-09-17 19:16:24 -0100

    refactor: move all protocol imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
+import { EmailValidator } from '../protocols'
 import { SignUpController } from './signup'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
 
 interface SutTypes {
   sut: SignUpController
